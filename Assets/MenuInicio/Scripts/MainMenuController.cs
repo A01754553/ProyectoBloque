de79f574df0ace0335fc18cad4b922c1ab39df5e
A01754553 <a01754553@tec.mx>
2026-04-25 19:37:59 -0600

    boton de creditos agregado y escena final con nuevo fondo

--- Assets/MenuInicio/Scripts/MainMenuController.cs
+++ Assets/MenuInicio/Scripts/MainMenuController.cs
@@ -6,26 +6,47 @@ public class MainMenuController : MonoBehaviour
 {
     private UIDocument _document;
     private Button _playButton;
+    private Button _botonCreditos;
+    private Button _botonCerrarCreditos;
+    private VisualElement _popUpCreditos;
+    private VisualElement _obscurecimiento;
     private AudioSource _audioSource;
 
-    void Awake()
+    void OnEnable()
     {
         _document = GetComponent<UIDocument>();
         _audioSource = GetComponent<AudioSource>();
 
         if (_document == null) return;
 
-        _playButton = _document.rootVisualElement.Q<Button>("playButton");
+        var root = _document.rootVisualElement;
 
-        if (_playButton == null) return;
+        _playButton = root.Q<Button>("playButton");
+        _botonCreditos = root.Q<Button>("BotonCreditos");
+        _botonCerrarCreditos = root.Q<Button>("BotonCerrarCreditos");
+        _popUpCreditos = root.Q<VisualElement>("PopUpCreditos");
+        _obscurecimiento = root.Q<VisualElement>("Obscurecimiento");
 
-        _playButton.RegisterCallback<ClickEvent>(OnPlayButtonClick);
+        if (_playButton != null)
+            _playButton.RegisterCallback<ClickEvent>(OnPlayButtonClick);
+
+        if (_botonCreditos != null)
+            _botonCreditos.RegisterCallback<ClickEvent>(AbrirCreditos);
+
+        if (_botonCerrarCreditos != null)
+            _botonCerrarCreditos.RegisterCallback<ClickEvent>(CerrarCreditos);
     }
 
     void OnDisable()
     {
         if (_playButton != null)
             _playButton.UnregisterCallback<ClickEvent>(OnPlayButtonClick);
+
+        if (_botonCreditos != null)
+            _botonCreditos.UnregisterCallback<ClickEvent>(AbrirCreditos);
+
+        if (_botonCerrarCreditos != null)
+            _botonCerrarCreditos.UnregisterCallback<ClickEvent>(CerrarCreditos);
     }
 
     private void OnPlayButtonClick(ClickEvent evt)
@@ -35,6 +56,20 @@ public class MainMenuController : MonoBehaviour
             _audioSource.Play();
 
         Invoke(nameof(LoadNextScene), 1f);
+    }
+
+    private void AbrirCreditos(ClickEvent evt)
+    {
+        // mostrar popup y obscurecimiento
+        _popUpCreditos.style.display = DisplayStyle.Flex;
+        _obscurecimiento.style.display = DisplayStyle.Flex;
+    }
+
+    private void CerrarCreditos(ClickEvent evt)
+    {
+        // ocultar popup y obscurecimiento
+        _popUpCreditos.style.display = DisplayStyle.None;
+        _obscurecimiento.style.display = DisplayStyle.None;
     }
 
     private void LoadNextScene()
